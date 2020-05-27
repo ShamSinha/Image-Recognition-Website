4631e207a4c719e5551b1af842c7cc782447c08f
SHUBHAM KUMAR <39132039+ShamSinha@users.noreply.github.com>
2020-05-27 15:44:26 +0530

    update file for tensorflow-gpu

--- inception_blocks_v2.py
+++ inception_blocks_v2.py
@@ -9,6 +9,8 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
 import fr_utils
 from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras import backend as K
+K.set_image_data_format('channels_first')
 
 def inception_block_1a(X):
     """
@@ -210,70 +212,73 @@ def inception_block_3b(X):
     return inception
 
 def faceRecoModel(input_shape):
-    """
+     """
     Implementation of the Inception model used for FaceNet
     
     Arguments:
     input_shape -- shape of the images of the dataset
-
     Returns:
     model -- a Model() instance in Keras
     """
+    with tf.device('/device:gpu:0'):
+        # Define the input as a tensor with shape input_shape
+        X_input = Input(input_shape)
+    
+        # Zero-Padding
+        X = ZeroPadding2D((3, 3))(X_input)
         
-    # Define the input as a tensor with shape input_shape
-    X_input = Input(input_shape)
-
-    # Zero-Padding
-    X = ZeroPadding2D((3, 3))(X_input)
-    
-    # First Block
-    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1')(X)
-    X = BatchNormalization(axis = 1, name = 'bn1')(X)
-    X = Activation('relu')(X)
-    
-    # Zero-Padding + MAXPOOL
-    X = ZeroPadding2D((1, 1))(X)
-    X = MaxPooling2D((3, 3), strides = 2)(X)
-    
-    # Second Block
-    X = Conv2D(64, (1, 1), strides = (1, 1), name = 'conv2')(X)
-    X = BatchNormalization(axis = 1, epsilon=0.00001, name = 'bn2')(X)
-    X = Activation('relu')(X)
-    
-    # Zero-Padding + MAXPOOL
-    X = ZeroPadding2D((1, 1))(X)
-
-    # Second Block
-    X = Conv2D(192, (3, 3), strides = (1, 1), name = 'conv3')(X)
-    X = BatchNormalization(axis = 1, epsilon=0.00001, name = 'bn3')(X)
-    X = Activation('relu')(X)
-    
-    # Zero-Padding + MAXPOOL
-    X = ZeroPadding2D((1, 1))(X)
-    X = MaxPooling2D(pool_size = 3, strides = 2)(X)
-    
-    # Inception 1: a/b/c
-    X = inception_block_1a(X)
-    X = inception_block_1b(X)
-    X = inception_block_1c(X)
-    
-    # Inception 2: a/b
-    X = inception_block_2a(X)
-    X = inception_block_2b(X)
-    
-    # Inception 3: a/b
-    X = inception_block_3a(X)
-    X = inception_block_3b(X)
-    
-    # Top layer
-    X = AveragePooling2D(pool_size=(3, 3), strides=(1, 1), data_format='channels_first')(X)
-    X = Flatten()(X)
-    X = Dense(128, name='dense_layer')(X)
-    
-    # L2 normalization
-    X = Lambda(lambda  x: K.l2_normalize(x,axis=1))(X)
-
-    # Create model instance
-    model = Model(inputs = X_input, outputs = X, name='FaceRecoModel')
+        # First Block
+        X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1')(X)
+        X = BatchNormalization(axis = 1, name = 'bn1')(X)
+        X = Activation('relu')(X)
         
-    return model
+        # Zero-Padding + MAXPOOL
+        X = ZeroPadding2D((1, 1))(X)
+        X = MaxPooling2D((3, 3), strides = 2)(X)
+        
+        # Second Block
+        X = Conv2D(64, (1, 1), strides = (1, 1), name = 'conv2')(X)
+        X = BatchNormalization(axis = 1, epsilon=0.00001, name = 'bn2')(X)
+        X = Activation('relu')(X)
+        
+        # Zero-Padding + MAXPOOL
+        X = ZeroPadding2D((1, 1))(X)
+    
+        # Second Block
+        X = Conv2D(192, (3, 3), strides = (1, 1), name = 'conv3')(X)
+        X = BatchNormalization(axis = 1, epsilon=0.00001, name = 'bn3')(X)
+        X = Activation('relu')(X)
+        
+        # Zero-Padding + MAXPOOL
+        X = ZeroPadding2D((1, 1))(X)
+        X = MaxPooling2D(pool_size = 3, strides = 2)(X)
+        
+        # Inception 1: a/b/c
+        X = inception_block_1a(X)
+        X = inception_block_1b(X)
+        X = inception_block_1c(X)
+        
+        # Inception 2: a/b
+        X = inception_block_2a(X)
+        X = inception_block_2b(X)
+        
+        # Inception 3: a/b
+        X = inception_block_3a(X)
+        X = inception_block_3b(X)
+        
+        # Top layer
+        X = AveragePooling2D(pool_size=(3, 3), strides=(1, 1), data_format='channels_first')(X)
+        X = Flatten()(X)
+        X = Dense(128, name='dense_layer')(X)
+        
+        # L2 normalization
+        X = Lambda(lambda  x: K.l2_normalize(x,axis=1))(X)
+    
+        # Create model instance
+        model = Model(inputs = X_input, outputs = X, name='FaceRecoModel')
+            
+        return model
+        
+   
+        
+        
