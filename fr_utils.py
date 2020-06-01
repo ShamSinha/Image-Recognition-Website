364d5e9009a8faa4f92b01049faec112e129f1e6
SHUBHAM KUMAR <39132039+ShamSinha@users.noreply.github.com>
2020-06-01 19:00:53 +0530

    update img_to_encoding

--- fr_utils.py
+++ fr_utils.py
@@ -159,12 +159,7 @@ def load_weights():
     return weights_dict
 
 
-def img_to_encoding(image_path, model):
-    img2 = cv2.imread(image_path, 1)
-    width = 96
-    height = 96
-    dim = (width, height)
-    img3 = cv2.resize(img2, dim)
+def img_to_encoding(img3, model):
     img = img3[...,::-1]
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
