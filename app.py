d532ab32424351d00abfbbb74ccd73c6adf783ce
manideepvuppala <42994406+manideepvuppala@users.noreply.github.com>
2020-06-13 20:52:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template,jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 
 #####
@@ -34,6 +34,9 @@ from tensorflow.keras.models import Model
 import cv2
 from fr_utils import *
 from inception_blocks_v2 import *
+from yolo_predict import *
+
+import matplotlib.pyplot as plt
 
 
 database = {}
@@ -53,17 +56,20 @@ FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
 load_weights_from_FaceNet(FRmodel)
 
 
-def who_is_it(face_path, database, model):
+def who_is_it(face, database, model):
     
-    encoding = img_to_encoding(face_path, model)
+    encoding = img_to_encoding(face, model)
        
     min_dist = 100
     for name in database:
-        dist = np.linalg.norm(encoding-database[name])
+        l=len(database[name])
+        dist=0
+        for i in range(0,l):
+            dist = dist+np.linalg.norm(encoding-database[name][i])/float(l)          
         if (dist<min_dist):
             min_dist = dist
             identity = name    
-    if min_dist > 0.9:
+    if min_dist > 2:
         identity="Not in database!!  "
         
     return min_dist, identity
@@ -72,72 +78,76 @@ def who_is_it(face_path, database, model):
 ########
 
 app = Flask(__name__)
+basepath = os.path.dirname(__file__)
+facedata = os.path.join(basepath, 'haarcascade_frontalface_default.xml')
 
-def SaveFace(file_path,basepath,personName,ext,directory):
-    
-    facedata = os.path.join(basepath, 'haarcascade_frontalface_default.xml')
+def returnFaces(image):  
     cascade = cv2.CascadeClassifier(facedata)   
-    img = cv2.imread(file_path)
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = cascade.detectMultiScale(gray_image,1.1, 4)
-    face_paths=[]
-    print(file_path)
-    i=0
-    for f in faces:
-        x, y, w, h = [ v for v in f ]
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
-        sub_face = img[y:y+h, x:x+w]
-        face_path_directory = os.path.join(basepath, directory)
-        os.chdir(face_path_directory)
-        if directory=='uploads' :
-            personName=personName+str(i)
-        cv2.imwrite(personName+"."+ext , sub_face)
-        face_paths.append(os.path.join(basepath, directory ,personName +"."+ext ))
-        i=i+1
-    print(face_paths)
-        
-    return face_paths
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray_image,1.1, 4)           
+    return faces
      
 
 @app.route('/', methods=['GET'])
 def index():
     basepath = os.path.dirname(__file__)
     directory = os.path.join(basepath, 'database')
-    for face_file in os.listdir(directory):
-        face_path = os.path.join(directory , face_file)
-        database[face_file.rsplit(".",1)[0]] = img_to_encoding(face_path , FRmodel)        
+    for face_dir in os.listdir(directory):
+        count=0
+        for face_file in os.listdir(os.path.join(directory,face_dir)): 
+            face_path = os.path.join(directory ,face_dir, face_file)
+            face=cv2.imread(face_path,1)
+            face=cv2.resize(face,(96,96))
+            if not (face_file.rsplit("_",1)[0] in database.keys()) :
+                database[face_file.rsplit("_",1)[0]] =[]
+            database[face_file.rsplit("_",1)[0]].append(img_to_encoding(face , FRmodel))        
     
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=[ 'GET','POST'])
 def upload():
     if request.method == 'POST':
+        if os.path.exists(os.path.join(basepath,'yolo',"temp1.jpg")):
+            os.remove(os.path.join(basepath,'yolo',"temp1.jpg"))
         f = request.files['file']
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join( basepath, 'uploads', secure_filename(f.filename))
+        filename=secure_filename(f.filename)
+        file_path = os.path.join( basepath, 'uploads',filename)
         f.save(file_path)
-        ext = f.filename.rsplit(".",1)[1]
+        img=our_result(file_path)
+        plt.imshow(img)
+        new_dir = os.path.join(basepath,'yolo')
+        #os.mkdir(new_dir)
+        #os.chdir(new_dir)
+        cv2.imwrite(os.path.join(new_dir,"temp1"+"."+"jpg"),img)
+        #img_url= os.path.join( basepath, 'yolo',"temp1.jpg")
+        img_url="yolo/temp1.jpg"
         result=""
-        face_paths = SaveFace(file_path,basepath,'unknown',ext,'uploads')
-        for face_path in face_paths:
-            min_dist,identity = who_is_it(face_path, database, FRmodel)
-            result = result+"\n"+str(identity)+ " "+str(min_dist)
-                
-            if os.path.exists(face_path):
-                os.remove(face_path)
+        image=cv2.imread(file_path,1)
+        faces = returnFaces(image)        
+        for face in faces: 
+            x, y, w, h = [ v for v in face ]
+            face_image=image[y:y+h, x:x+w]
+            face_image=cv2.resize(face_image,(96,96))
+            min_dist,identity = who_is_it(face_image, database, FRmodel)
+            result = result+str(identity)+ " "
         if os.path.exists(file_path):
             os.remove(file_path)
-        return result
+        return jsonify({"result": result , "our_url" : img_url })
     return None
 
-@app.route('/add', methods=['GET'])
+@app.route('/add', methods=['GET','POST'])
 def addDatabase():
     return render_template('addDatabase.html')
 
-@app.route('/remove', methods=['GET'])
-def removeDatabase():
-    return render_template('removeDatabase.html')
+@app.route('/yolo/temp1.jpg', methods=['GET','POST'])
+def for_img():
+    return send_from_directory("yolo/", "temp1.jpg")
+'''
+@app.route('/my_json', methods=['GET'])
+def Json():
+    return jsonify({"result":"hi"})
+'''
 
 @app.route('/addStatus', methods =['GET','POST'])
 def add():
@@ -145,40 +155,90 @@ def add():
         personImage = request.files['image']
         personName = request.form['personName']
         personName = personName.lower()
-        
-        basepath = os.path.dirname(__file__)
-        
-        ext = personImage.filename.rsplit(".",1)[1]
-        file_path = os.path.join(basepath, 'database', "unknown" +"."+ext)
+        filename=personImage.filename
+        file_path = os.path.join(basepath, 'database', filename)
         personImage.save(file_path)
-        face_path = SaveFace(file_path,basepath,personName,ext,'database')
-        database[personName] = img_to_encoding(face_path[0] , FRmodel)
+        image=cv2.imread(file_path,1)
+        faces = returnFaces(image)
+        global database
+        if not (personName in database):
+            new_dir = os.path.join(basepath,'database',personName)
+            face=faces[0]
+            x, y, w, h = [ v for v in face ]
+            face_image=image[y:y+h, x:x+w]
+            os.mkdir(new_dir)
+            os.chdir(new_dir)
+            cv2.imwrite(personName+"_"+str(1)+"."+"jpg", face_image)
+            database[personName]=[]
+            face_image=cv2.resize(face_image,(96,96))
+            database[personName].append(img_to_encoding(face_image , FRmodel))
+        else :
+            face=faces[0]
+            x, y, w, h = [ v for v in face ]
+            face_image=image[y:y+h, x:x+w]
+            l=len(database[personName])
+            new_dir = os.path.join(basepath,'database',personName)
+            os.chdir(new_dir)
+            cv2.imwrite(personName+"_"+str(l)+"."+"jpg", face_image)
+            face_image=cv2.resize(face_image,(96,96))
+            database[personName].append(img_to_encoding(face_image, FRmodel))
         if os.path.exists(file_path):
             os.remove(file_path)
         result = personName + " Successfully added in Database"
-            
-    return render_template('addStatus.html', result = result)
+        print(result) 
+        return jsonify({"result": result})
+    return None
 
 
-@app.route('/removeStatus', methods =['GET','POST'])
+
+
+@app.route('/database', methods = ['GET' , 'POST'])
+def show():
+    basepath = os.path.dirname(__file__)
+    person_names = os.listdir(os.path.join(basepath,'database'))
+    
+    return render_template('showdatabase.html', person_names = person_names)
+  
+@app.route('/showAllInDatabase/<person>', methods = ['GET' , 'POST'])
+def showAll(person):
+    basepath = os.path.dirname(__file__)
+    image_names = os.listdir(os.path.join(basepath, 'database',person))
+    
+    return render_template('person_database.html', image_names = image_names , person = person)
+
+@app.route('/showAllInDatabase/<person>/<filename>')
+def send_all_image(person,filename):
+    return send_from_directory("database/"+ person, filename)
+
+
+
+@app.route("/removed", methods=["GET", "POST"])
 def remove():
     if request.method == 'POST':
-        personName = request.form['personName']
-        personName = personName.lower()
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'database', personName +"."+"jpg")
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            del database[personName]
-            result = personName + " Successfully removed from Database"
-        else:
-            result = "The person is not in Database!!"
-            
-        
-            
-    return render_template('removeStatus.html', result = result)
-        
+        for key, value in request.form.items():
+            if key == "personName":
+                person_name = value
+                for person in os.listdir(os.path.join(basepath,'database',person_name)):
+                    if(os.path.exists(os.path.join(basepath,'database',person_name,person))):
+                        os.remove(os.path.join(basepath,'database',person_name,person))
+                os.rmdir(os.path.join(basepath,'database',person_name))
+                            
+                list_database = database.keys();
+                list_database_to_be_removed = []
+                for i in list_database:
+                    if i.rsplit("_",1)[0] == person_name:
+                        list_database_to_be_removed.append(i)
+                        
+                for j in list_database_to_be_removed:
+                    del database[j]
+                 
+                #result = person_name.upper() + " Successfully removed from Database"
       
+    basepath = os.path.dirname(__file__)
+    person_names = os.listdir(os.path.join(basepath,'database'))
+    
+    return render_template('showdatabase.html', person_names = person_names)
 
 if __name__ == '__main__':
     app.run(debug =True)
