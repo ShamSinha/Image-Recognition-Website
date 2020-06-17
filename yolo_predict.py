282a52fcd76c3e6331c3a7d35edb4f9c97556218
manideepvuppala <42994406+manideepvuppala@users.noreply.github.com>
2020-06-17 20:41:49 +0530

    Update yolo_predict.py

--- yolo_predict.py
+++ yolo_predict.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun  3 11:13:43 2020
-
 @author: manideep
 """
 
@@ -16,6 +15,8 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
+import cv2
+import os
 
 class BoundBox:
     def __init__(self, xmin, ymin, xmax, ymax, objness = None, classes = None):
@@ -168,59 +169,66 @@ def draw_boxes(filename, v_boxes, v_labels, v_scores):
 		# calculate width and height of the box
         width, height = x2 - x1, y2 - y1
 		# create the shape
-        rect = Rectangle((x1, y1), width, height, fill=False, color='white')
+        rect = Rectangle((x1, y1), width, height, fill=False, color='black')
 		# draw the box
         ax.add_patch(rect)
 		# draw text and score in top left corner
         label = "%s (%.3f)" % (v_labels[i], v_scores[i])
-        pyplot.text(x1, y1, label, color='white')
-    pyplot.savefig('zebra_detect.jpg')
-	# show the plot
+        pyplot.text(x1, y1, label, color='black')
+    ax.set_axis_off()
+    
+    pyplot.savefig('yolo/temp_detect.jpg',bbox_inches='tight')
+    img=cv2.imread('yolo/temp_detect.jpg')
+    if os.path.exists('yolo/temp_detect.jpg'):
+            os.remove('yolo/temp_detect.jpg')
     pyplot.show()
+    return img
+	
     
    
-    
-# load yolov3 model
-model = load_model('model.h5')
-# define the expected input shape for the model
-input_w, input_h = 416, 416
-# define our new photo
-photo_filename = 'photos/zebra.jpg'
-# load and prepare image
-image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
-image=np.transpose(image,(0,2,3,1))
-# make prediction
-yhat = model.predict(image)
-# summarize the shape of the list of arrays
-#print([a.shape for a in yhat])
-# define the anchors
-anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
-# define the probability threshold for detected objects
-class_threshold = 0.6
-boxes = list()
-for i in range(len(yhat)):
-	# decode the output of the network
-	boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
-# correct the sizes of the bounding boxes for the shape of the image
-correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
-# suppress non-maximal boxes
-do_nms(boxes, 0.5)
-# define the labels
-labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
-	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
-	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
-	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
-	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
-	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
-	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
-	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
-# get the details of the detected objects
-v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
-# summarize what we found
-for i in range(len(v_boxes)):
-    #print(v_labels[i], v_scores[i])
-    pass
-# draw what we found
-draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+def our_result(filename):    
+    # load yolov3 model
+    model = load_model('model.h5')
+    # define the expected input shape for the model
+    input_w, input_h = 416, 416
+    # define our new photo
+    photo_filename = filename
+    # load and prepare image
+    image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
+    image=np.transpose(image,(0,2,3,1))
+    # make prediction
+    yhat = model.predict(image)
+    # summarize the shape of the list of arrays
+    #print([a.shape for a in yhat])
+    # define the anchors
+    anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+    # define the probability threshold for detected objects
+    class_threshold = 0.6
+    boxes = list()
+    for i in range(len(yhat)):
+    	# decode the output of the network
+    	boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
+    # correct the sizes of the bounding boxes for the shape of the image
+    correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
+    # suppress non-maximal boxes
+    do_nms(boxes, 0.5)
+    # define the labels
+    labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+    	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+    	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+    	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+    	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+    	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+    	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+    	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+    	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+    	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+    # get the details of the detected objects
+    v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+    # summarize what we found
+    for i in range(len(v_boxes)):
+        #print(v_labels[i], v_scores[i])
+        pass
+    # draw what we found
+    img=draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+    return img
